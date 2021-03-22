de0e70bae029fee08a524f90d58945c457e756ba
sylvainJnt29 <sylvain.jnt@gmail.com>
2021-03-22 15:02:55 +0100

    conditions

---
+++ conditions/vue.js
@@ -0,0 +1,21 @@
+new Vue({
+    el: '#app',
+    data:{
+        show: false
+    }
+});
+
+new Vue({
+    el: '#app2',
+    data:{
+        show: true,
+        color: 'blue'
+    }
+});
+
+new Vue({
+    el: '#app3',
+    data:{
+        show: true
+    }
+});
